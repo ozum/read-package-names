b93ff597f69b5ac3d5623e49f4da9fc2e48824f2
Özüm Eldoğan <ozum@ozum.net>
2021-02-20 22:03:06 +0300

    fix: fix test helper files directory name

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,19 +1,21 @@
 import { join } from "path";
 import readPackageNames from "../src";
 
+const DIR = join(__dirname, "test_helper/module_dir");
+
 describe("readPackageNames", () => {
   it("should read package names.", async () => {
-    const packages = await readPackageNames(join(__dirname, "test_helper/node_modules"));
+    const packages = await readPackageNames(DIR);
     expect(packages.sort()).toEqual(["@s/x", "@s/y", "a", "b"]);
   });
 
   it("should read scoped package names.", async () => {
-    const packages = await readPackageNames(join(__dirname, "test_helper/node_modules"), { scope: "@s" });
+    const packages = await readPackageNames(DIR, { scope: "@s" });
     expect(packages.sort()).toEqual(["@s/x", "@s/y"]);
   });
 
   it("should read scoped package names without '@' sign.", async () => {
-    const packages = await readPackageNames(join(__dirname, "test_helper/node_modules"), { scope: "s" });
+    const packages = await readPackageNames(DIR, { scope: "s" });
     expect(packages.sort()).toEqual(["@s/x", "@s/y"]);
   });
 });
